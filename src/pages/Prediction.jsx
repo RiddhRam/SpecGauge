0ba2d496510fcf0018eb37f4fefd8421422fafab
ridderp <ridderp05@gmail.com>
2024-07-30 13:06:43 -0400

    Fixed a bug where rng wasn't working properly in prediction screens

--- src/pages/Prediction.jsx
+++ src/pages/Prediction.jsx
@@ -190,7 +190,7 @@ export default function Prediction({
     }
   };
 
-  function addToGraph(priceString, yearString, brand) {
+  async function addToGraph(priceString, yearString, brand) {
     const price = parseFloat(priceString);
     const year = parseFloat(yearString);
 
@@ -272,17 +272,16 @@ export default function Prediction({
       }
       // If rate is depreciating, follow these rules
       // For each year it was released, calculate the price for that year
-      // Get the rng value
+      // Get the rng() value
       const seed = `${price}${brand}${year}${lastPrice}${rateAdjustments}`;
       let rng = null;
-      import("../functions/SeedrandomImport").then((module) => {
-        const rngFunc = module.default(seed);
-        rng = rngFunc();
+      await import("../functions/SeedrandomImport").then((module) => {
+        rng = module.default(seed);
       });
 
       // Get the new price
       let newCalculatedPrice =
-        price * Math.E ** ((rate + rng * 0.02) * (i - year));
+        price * Math.E ** ((rate + rng() * 0.02) * (i - year));
       // Difference between price of last iteration and this one
       let difference = newCalculatedPrice - lastPrice;
 
@@ -297,7 +296,7 @@ export default function Prediction({
       // If last price was greater than 1.5x the original value, and last price wasn't an increase
       if (lastPrice > originalPrice * 1.5 && !lastPriceIncreased) {
         // The price will hover around twice it's original value
-        const differenceRate = rng * -0.04;
+        const differenceRate = rng() * -0.04;
         difference = lastPrice * differenceRate;
         lastPriceIncreased = false;
       }
@@ -327,14 +326,14 @@ export default function Prediction({
       else {
         // Difference is reduced to 2% from the last price if rate isn't positive
         if (rate < 0) {
-          difference = lastPrice * rng * -0.06;
+          difference = lastPrice * rng() * -0.06;
           lastPriceIncreased = false;
         }
       }
 
       /* If difference is signifcantly larger than original price, maybe because rate was too high, bring it down to within 10% of the original price */
       if (difference + lastPrice > originalPrice * 2.1) {
-        difference = originalPrice * 0.1 * rng;
+        difference = originalPrice * 0.1 * rng();
       }
       prices.push(lastPrice + difference);
       lastPrice = lastPrice + difference;
@@ -938,8 +937,12 @@ export default function Prediction({
 
                 {/* Add */}
                 <button
-                  onClick={() => {
-                    const result = addToGraph(initialPrice, releaseYear, brand);
+                  onClick={async () => {
+                    const result = await addToGraph(
+                      initialPrice,
+                      releaseYear,
+                      brand
+                    );
                     if (result != 0) {
                       setError(result);
                       setShowErrorModal(true);
@@ -1247,8 +1250,12 @@ export default function Prediction({
 
               {/* Add */}
               <button
-                onClick={() => {
-                  const result = addToGraph(initialPrice, releaseYear, brand);
+                onClick={async () => {
+                  const result = await addToGraph(
+                    initialPrice,
+                    releaseYear,
+                    brand
+                  );
                   if (result != 0) {
                     setError(result);
                     setShowErrorModal(true);
